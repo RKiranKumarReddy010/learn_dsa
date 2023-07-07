1a9534104f0ec92d483624d2b7e69c57dd2456c6
UZUMAKI NARUTO <ITACHI@ITACHI-PC>
2023-07-07 23:07:20 +0530

    commit

--- dsa3.js
+++ dsa3.js
@@ -1,5 +1,15 @@
-
+var cur = document.querySelector("#cursor")
+var blurr = document.querySelector("#cursor-blur")
+document.addEventListener("mousemove",function(dets){
+    cur.style.left = dets.x- 5 +"px"
+    cur.style.top = dets.y- 5 +"px"
+})
+document.addEventListener("mousemove",function(dets){
+    blurr.style.left = dets.x - 150 +"px"
+    blurr.style.top = dets.y- 150 +"px"
+})
 gsap.to("#nav",{
+    backgroundColor:"#000",
     duration:0.5,
     height:"130px",
     scrollTrigger:{
@@ -8,6 +18,7 @@ gsap.to("#nav",{
         markers:true,
         start:"top -10%",
         end:"top -11%",
+        scrub:true,
     }
 })
 gsap.to("#main",{
@@ -15,8 +26,9 @@ gsap.to("#main",{
     scrollTrigger:{
         trigger:"#main",
         scroller:"body",
-        markers:true,
-        start: "top -50%",
-        end:"top -100%"
+        start: "top -30%",
+        end:"top -100%",
+        scrub:2,
     }
 })
+
